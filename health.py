1e7804fc4cdceb0119acf40d19103070ab49da91
Mohdtalha01 <mtalha.tyagi@gmail.com>
2024-09-03 19:22:22 +0530

    Interface Updated

--- health.py
+++ health.py
@@ -11,7 +11,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 def get_gemini_response(input,image,prompt):
-    model=genai.GenerativeModel("gemini-pro-vision")
+    model=genai.GenerativeModel("gemini-1.5-pro")
     response=model.generate_content([input,image[0],prompt])
     return response.text
 
